7b385829e89581f8cfac6a77153cce983dc50acb
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-26 13:33:02 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,43 +9,37 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *tmp, *head;
+
 	if (!list)
 		return;
 	head = *list;
 	curr = head;
 	while (head)
 	{
-		if (curr->prev)
+		if (curr->prev && curr->n < curr->prev->n)
 		{
-			if (curr->n < curr->prev->n)
-			{
-				tmp = curr->prev;
-				tmp->next = curr->next;
-				curr->prev = tmp->prev;
-				if (tmp->prev)
-					tmp->prev->next = curr;
-				else
-					*list = curr;
-				if (curr->next)
-					curr->next->prev = tmp;
-				tmp->prev = curr;
-				curr->next = tmp;
-				print_list(*list);
-			}
+			tmp = curr->prev;
+			tmp->next = curr->next;
+			curr->prev = tmp->prev;
+			if (tmp->prev)
+				tmp->prev->next = curr;
 			else
-			{
-				if (head->next != NULL)
-				{
-					head = head->next;
-					curr = head;
-				}
-				else
-					return;
-			}
+				*list = curr;
+			if (curr->next)
+				curr->next->prev = tmp;
+			tmp->prev = curr;
+			curr->next = tmp;
+			print_list(*list);
 		}
 		else
 		{
-			head = head->next;
-			curr = head;
+			if (head->next)
+			{
+				head = head->next;
+				curr = head;
+			}
+			else
+				return;
 		}
+	}
 }
