6d51ea6ac29510b8d01fe13d23f17cd7edac0adf
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-26 16:27:50 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, k;
-    int tmp;
+	size_t i, j, k;
+	int tmp;
 
-    for (i = 0; i < size; i++)
-    {
-        tmp = array[i];
-        for (j = i + 1; j < size; j++)
-        {
-            if (tmp > array[j])
-            {
-                tmp = array[j];
-                k = j;
-            }
-        }
-        if (tmp != array[i])
-        {
-            array[k] = array[i];
-            array[i] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		tmp = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (tmp > array[j])
+			{
+				tmp = array[j];
+				k = j;
+			}
+		}
+		if (tmp != array[i])
+		{
+			array[k] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
 }
