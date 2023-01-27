dfbe16806770a9366f2525a840efed85b17f7e4c
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-27 01:19:15 +0100

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts a array of integers using
+ * selection method.
+ * @array: pointer to the array of integers.
+ * @size: size of the array.
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, k, tmp;
+
+    if (!array)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        tmp = array[i];
+        for (j = i + 1; j < size; j++)
+        {
+            if (tmp > array[j])
+            {
+                tmp = array[j];
+                k = j;
+            }
+        }
+        if (tmp != array[i])
+        {
+            array[k] = array[i];
+            array[i] = tmp;
+            print_array(array, size);
+        }
+    }
+}
